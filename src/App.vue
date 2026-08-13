0cfe9ba5e590e7a18a6717f461355e4aeb4a05f8
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2026-08-13 00:21:18 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Recipients List Manager</h1>
+  </main>
+</template>
+
+<style lang="scss">
+main {
+  padding: 2rem;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+})
